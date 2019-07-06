1f766158d1417eff0fc940327bdbef24361e3e8d
QueenCurry <yeawonjo@gmail.com>
2019-07-06 14:37:34 +0900

    fix error and import react-native elements

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React, { Components } from 'react'
+import { Button, ThemeProvider } from 'react-native-elements';
+
+export default class LoginScreen extends Components{
+    render(){
+        <ThemeProvider>
+            <Button title="Hey!" />
+        </ThemeProvider>
+    }
+}
--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { AppRegistry, Image, TouchableOpacity, View } from 'react-native';
 
 export default class MainScreen extends Component {
-    callFun = () => {alret("change screen to mainscreen!") ;}
+    //callFun = () => {alret("change screen to mainscreen!") ;}
 
     render() {
+        /*
         let logo = {
-            uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/LOGO.png'
+            //uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/LOGO.png'
+            uri: "../"
         };
         let subtitle = {
             uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/SUBTITLE.png'
@@ -14,12 +16,17 @@ export default class MainScreen extends Component {
         let start = {
             uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/STARTBUTTON.png'
         };
+        */
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Image source={logo} style={{ alignItems: 'center' }} />
-            <Image source={subtitle} style={{ alignItems: 'center' }} />
+            <Image source={require('../assets/LOGO.png')} 
+                style={{ alignItems: 'center' }} />
+            <Image source={require('../assets/SUBTITLE.png')} 
+                style={{ alignItems: 'center' }} />
             <TouchableOpacity activeOpacity={0.5} onPress={this.callFun}>
-                <Image source={start} style={styles.ImageClass}/>
+                <Image source={require('../assets/STARTBUTTON')} 
+                    //style={styles.ImageClass}/>
+                    style={{ alignItems: 'center' }}/>
             </TouchableOpacity>
       </View>
         );
