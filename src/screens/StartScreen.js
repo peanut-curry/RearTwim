98ed212836cf80c3e2edef008efa0195bd03f987
QueenCurry <yeawonjo@gmail.com>
2019-07-06 14:53:39 +0900

    fix error

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -24,7 +24,7 @@ export default class MainScreen extends Component {
             <Image source={require('../assets/SUBTITLE.png')} 
                 style={{ alignItems: 'center' }} />
             <TouchableOpacity activeOpacity={0.5} onPress={this.callFun}>
-                <Image source={require('../assets/STARTBUTTON')} 
+                <Image source={require('../assets/STARTBUTTON.png')} 
                     //style={styles.ImageClass}/>
                     style={{ alignItems: 'center' }}/>
             </TouchableOpacity>
