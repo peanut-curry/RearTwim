37509993a6a0c0313f278b8fcc5013ebf8a74338
PARK, YUJIN <jinee525@naver.com>
2019-07-06 14:44:42 +0900

    Update StartScreen.js

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -5,18 +5,6 @@ export default class MainScreen extends Component {
     //callFun = () => {alret("change screen to mainscreen!") ;}
 
     render() {
-        /*
-        let logo = {
-            //uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/LOGO.png'
-            uri: "../"
-        };
-        let subtitle = {
-            uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/SUBTITLE.png'
-        };
-        let start = {
-            uri: 'https://github.com/peanut-curry/Twim/blob/master/src/assets/STARTBUTTON.png'
-        };
-        */
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Image source={require('../assets/LOGO.png')} 
@@ -25,7 +13,6 @@ export default class MainScreen extends Component {
                 style={{ alignItems: 'center' }} />
             <TouchableOpacity activeOpacity={0.5} onPress={this.callFun}>
                 <Image source={require('../assets/STARTBUTTON')} 
-                    //style={styles.ImageClass}/>
                     style={{ alignItems: 'center' }}/>
             </TouchableOpacity>
       </View>
