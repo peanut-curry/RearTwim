feeeea6c316ac669448de0c7cd40a29758f480e1
QueenCurry <yeawonjo@gmail.com>
2019-07-19 21:54:18 +0900

    일지로컬DB추가, 로그인시Main에서시작구현, 내위치로지도옮기기구현

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,49 +1,61 @@
 import React, { Component } from 'react';
 import { Image, Button } from 'react-native-elements';
 import { View, StyleSheet, Text, StatusBar, Dimensions, ToastAndroid } from 'react-native';
-import { GoogleSignin } from 'react-native-google-signin';
-import firebase from 'react-native-firebase'
+import { GoogleSignin, statusCodes } from 'react-native-google-signin';
+import firebase from 'react-native-firebase';
+import { AsyncStorage } from "react-native";
 
 export default class LoginScreen extends Component{
     static navigationOptions = {
         header: null,
     };
 
-    naverLoginClicked = () => {
-        //네이버 로그인 코드 넣기
-        //ToastAndroid.show("네이버 로그인은 준비중", ToastAndroid.SHORT);
-        this.props.navigation.navigate('Main');
-    }
-
     googleLoginClicked = async() => {
         try {
-          await GoogleSignin.configure({
-            webClientId: "971126083783-ii32s55udvr060d2e6rgcv05h23mu95l.apps.googleusercontent.com",
-            offlineAccess: false,
-          });
+            //로그인 로딩 컴포넌트 띄우기  
+            await GoogleSignin.configure({
+                webClientId: "971126083783-ii32s55udvr060d2e6rgcv05h23mu95l.apps.googleusercontent.com",
+                offlineAccess: false,
+            });
 
-          if (await GoogleSignin.isSignedIn()){
-            ToastAndroid.show("이미 구글 계정으로 로그인 되었습니다.", ToastAndroid.SHORT);
-            this.props.navigation.navigate('Main');
-          }
-          else{
-              const data = await GoogleSignin.signIn();
-              if (data==null){
-                ToastAndroid.show("구글 계정으로 로그인 중 문제가 발생했습니다.\n 다시 시도해주세요.", ToastAndroid.SHORT);
-                return;
-              }
-              //Send Credential
-              const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
-              const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
-              
-              console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
-              this.props.navigation.navigate('Main'); 
-          }
+            if (await GoogleSignin.isSignedIn()){
+                ToastAndroid.show("이미 구글 계정으로 로그인 되었습니다.", ToastAndroid.SHORT);
+                this.props.navigation.navigate('Main');
+            }
+            else{
+                const data = await GoogleSignin.signIn();
+                if (data==null){
+                    ToastAndroid.show("구글 계정으로 로그인 중 문제가 발생했습니다.\n 다시 시도해주세요.", ToastAndroid.SHORT);
+                }
+                else{
+                    //Send Credential
+                    const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+                    const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
+                    
+                    //1.첫 로그인이라면,
+                    await AsyncStorage.setItem("isTraveling", "false"); //여행중 아님
+
+                    /*2. 첫 로그인이 아니라면
+                    UserId를 이용해서 클라우드에서 유저의 기존 일지와 프로필을 동기화해야함.
+                    기존 일지의 마지막 여행을 확인해서, AsyncStorage를 설정해야함.
+                    */
+
+                    //로딩중 컴포넌트 지우기
+                    this.props.navigation.navigate('Main'); 
+                }
+            }
     
         } catch (e) {
-          console.error(e);
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                ToastAndroid.show("이미 로그인이 진행중입니다.",ToastAndroid.SHORT);} 
+            else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                ToastAndroid.show("Twim을 이용하려면 GooglePlayService가 활성화되어야 합니다.",ToastAndroid.SHORT);
+            } else{
+                ToastAndroid.show("구글 계정으로 로그인 중 문제가 발생했습니다.\n 다시 시도해주세요.", ToastAndroid.SHORT);
+            }
         }
-      }
+    }
     
     render(){
         var {height, width} = Dimensions.get('window');
@@ -59,8 +71,6 @@ export default class LoginScreen extends Component{
                     <Text style={{textAlign:'center', fontSize:20, textAlignVertical:'center', marginTop:textMargin}}>Travel with map,{'\n'}Travel with me,{'\n'}twim!</Text>
                 </View>
                 <View style={loginStyles.bottomContainer}>
-                    <Button buttonStyle={loginStyles.naverLoginButton} containerStyle={{width:buttonWidth, marginBottom:textMargin}}
-                        title="네이버로 로그인하기" onPress={this.naverLoginClicked}></Button>
                     <Button buttonStyle={loginStyles.googleLoginButton} containerStyle={{width:buttonWidth}}
                         title="Google로 로그인하기" onPress={this.googleLoginClicked}></Button>
                 </View>
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, StatusBar, View, Image, TouchableOpacity, Dimensions, PermissionsAndroid } from 'react-native';
+import { StyleSheet, StatusBar, View, Image, TouchableOpacity, Dimensions, 
+    AsyncStorage, ToastAndroid } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class MainScreen extends Component{
-    componentDidMount() {
-        PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-              title: "Location Accessing Permission",
-              message: "App needs access to your location"
-            }
-          );
-        console.log("위치 가져오기 시작");
+    constructor(){
+        super();
+        this.state = {
+            isLoading: true,
+            isTraveling: false,
+            region : []
+        }
+    }
+
+    goToCurLocation = () => {
         navigator.geolocation.getCurrentPosition(
-          function(position) {
-            console.log(position);
-          }, 
-          function(error) {
-            console.log(error);
-          },
-          {enableHighAccuracy:false, timeout:50000}
+            (position) => {
+                var currentLongitude = parseFloat(JSON.stringify(position.coords.longitude));
+                var currentLatitude = parseFloat(JSON.stringify(position.coords.latitude));
+                this.setState({
+                    isLoading: false,
+                    isTraveling: false,
+                    region: {
+                        latitude: currentLatitude,
+                        longitude: currentLongitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }
+                })
+            }, 
+            (error) => {
+                ToastAndroid.show("현재 위치를 불러오는데 오류 발생. 다시 시도해주세요", ToastAndroid.SHORT);
+            },
+            {enableHighAccuracy:false, timeout:1000}
         );
-      }
+    }
+
+    componentDidMount = async() => {
+        var isTraveling = await AsyncStorage.getItem("isTraveling'");
+        if (isTraveling=='true'){ //여행중인 경우
+            //최근 일지 가져와서 State에 넣기.
+        } 
+        else{ //여행 중 아닌 경우
+            //현재 위치 가져오기.
+            this.goToCurLocation();
+        }   
+    }
 
     static navigationOptions = ({ navigation }) => {
         var iconSize = ((Dimensions.get('window').height/9.0)-StatusBar.currentHeight)*0.6;
@@ -37,29 +61,30 @@ export default class MainScreen extends Component{
       };
 
     render(){
-        return(
-            <View style={MainStyles.container}>
-                <StatusBar backgroundColor={'transparent'} translucent={true} hidden={false} barStyle='dark-content'></StatusBar>
-                    <MapView style={MainStyles.map} provider={PROVIDER_GOOGLE}
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }} />
-
-                    <ActionButton buttonColor="rgba(156,39,176,100)">     
-                        <ActionButton.Item buttonColor='#1abc9c' title="여행 시작!" onPress={() => { }}>
-                            <Icon src="../assets/map.svg" style={styles.actionButtonIcon} />
-                        </ActionButton.Item>
-                </ActionButton>
-
-               
-            </View>
-        );
+        if(this.state.isLoading){
+            //로딩 컴포넌트 띄우기
+            return null;
+        }
+        else{
+            if(this.state.isTraveling){ //1.여행중이라면,
+                //여행 일지 정보 기반으로 polyline 띄우기
+            }else{ //2.여행중이 아니라면,
+                return(
+                    <View style={MainStyles.container}>
+                        <StatusBar backgroundColor={'transparent'} translucent={true} hidden={false} barStyle='dark-content'></StatusBar>
+                            <MapView style={MainStyles.map} provider={PROVIDER_GOOGLE}
+                                region={this.state.region}/>
+                            <ActionButton buttonColor="rgba(156,39,176,100)">     
+                                <ActionButton.Item buttonColor='#1abc9c' title="여행 시작!" onPress={() => { }}>
+                                    <Icon src="../assets/map.svg" style={styles.actionButtonIcon} />
+                                </ActionButton.Item>
+                        </ActionButton>
+                    </View>
+                );
+            }
+        }
     }
 }
-
 
 const MainStyles = StyleSheet.create({
     container:{
